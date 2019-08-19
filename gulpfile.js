0f84edca2b017ce709e27f916e60dfa4462f5be4
lele <lele18342960091@outlook.com>
2019-08-19 14:19:58 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp')
+// 重新命名
+const rename = require('gulp-rename')
+// js文件压缩
+const uglify = require('gulp-uglify')
+// css文件压缩
+const cssMin = require('gulp-clean-css')
+// js转换
+const jsBabel = require('gulp-babel')
+// css转换
+const cssBabel = require('gulp-sass')
